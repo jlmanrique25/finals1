1ae0f8c0595fa086a437741bed046e62b467ad60
Jasffer Padigdig <jasfferp@gmail.com>
2021-11-11 09:28:05 +0800

    project function is done, need EJB

--- src/main/java/backend.java
+++ src/main/java/backend.java
@@ -1,4 +1,5 @@
 import jakarta.annotation.ManagedBean;
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Named;
 
@@ -6,10 +7,100 @@ import java.io.Serializable;
 import java.util.*;
 
 @ManagedBean
-@SessionScoped
+@RequestScoped
 @Named
-public class backend implements Serializable {
+public class backend{
     private String move;
+    private long board_id;
+    private String cell1;
+    private String cell2;
+    private String cell3;
+    private String cell4;
+    private String cell5;
+    private String cell6;
+    private String cell7;
+    private String cell8;
+    private String cell9;
+
+    public long getBoard_id() {
+        return board_id;
+    }
+
+    public void setBoard_id(long board_id) {
+        this.board_id = board_id;
+    }
+
+    public String getCell1() {
+        return cell1;
+    }
+
+    public void setCell1(String cell1) {
+        this.cell1 = cell1;
+    }
+
+    public String getCell2() {
+        return cell2;
+    }
+
+    public void setCell2(String cell2) {
+        this.cell2 = cell2;
+    }
+
+    public String getCell3() {
+        return cell3;
+    }
+
+    public void setCell3(String cell3) {
+        this.cell3 = cell3;
+    }
+
+    public String getCell4() {
+        return cell4;
+    }
+
+    public void setCell4(String cell4) {
+        this.cell4 = cell4;
+    }
+
+    public String getCell5() {
+        return cell5;
+    }
+
+    public void setCell5(String cell5) {
+        this.cell5 = cell5;
+    }
+
+    public String getCell6() {
+        return cell6;
+    }
+
+    public void setCell6(String cell6) {
+        this.cell6 = cell6;
+    }
+
+    public String getCell7() {
+        return cell7;
+    }
+
+    public void setCell7(String cell7) {
+        this.cell7 = cell7;
+    }
+
+    public String getCell8() {
+        return cell8;
+    }
+
+    public void setCell8(String cell8) {
+        this.cell8 = cell8;
+    }
+
+    public String getCell9() {
+        return cell9;
+    }
+
+    public void setCell9(String cell9) {
+        this.cell9 = cell9;
+    }
 
     public String getMove() {
         return move;
@@ -23,10 +114,18 @@ public class backend implements Serializable {
 
     }
 
-    public backend(String move) {
-        this.move = move;
+    public backend(Long board_id, String cell1, String cell2, String cell3, String cell4, String cell5, String cell6, String cell7, String cell8, String cell9) {
+        this.board_id = board_id;
+        this.cell1 = cell1;
+        this.cell2 = cell2;
+        this.cell3 = cell3;
+        this.cell4 = cell4;
+        this.cell5 = cell5;
+        this.cell6 = cell6;
+        this.cell7 = cell7;
+        this.cell8 = cell8;
+        this.cell9 = cell9;
     }
-
 
     @Override
     public String toString() {
--- src/main/java/functions.java
+++ src/main/java/functions.java
@@ -1,29 +1,73 @@
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Named;
 
+import java.io.Serializable;
 import java.util.*;
-@RequestScoped
+@SessionScoped
 @Named
-public class functions {
+public class functions implements Serializable {
+
     List<String> board = Arrays.asList("1","2","3","4","5","6","7","8","9");
     private String winner;
     private backend move = new backend();
+    private List<backend> form_board = new ArrayList<>();
 
+    public String mod_board(){
+        long board_id =form_board.size() + 1;
+        this.form_board.add(new backend(board_id, this.move.getCell1(), this.move.getCell2(), this.move.getCell3(), this.move.getCell4(), this.move.getCell5(), this.move.getCell6(), this.move.getCell7(), this.move.getCell8(),this.move.getCell9()));
 
+        this.form_board.stream().forEach(item->{
+            System.out.println(form_board.toString());
+        });
+        return "Some string";
+    }
+
+    public void set_items(){
+        move.setCell1(board.get(0));
+        move.setCell2(board.get(1));
+        move.setCell3(board.get(2));
+        move.setCell4(board.get(3));
+        move.setCell5(board.get(4));
+        move.setCell6(board.get(5));
+        move.setCell7(board.get(6));
+        move.setCell8(board.get(7));
+        move.setCell9(board.get(8));
+    }
 
     public String main_method(){
+//        if (form_board == null){
+//            List<String> board = Arrays.asList("1","2","3","4","5","6","7","8","9");
+//        }else{
+//            //List<String> board = Arrays.asList(move.getCell1(),move.getCell2(),move.getCell3(),move.getCell4(),move.getCell5(),move.getCell6(),move.getCell7(),move.getCell8(),move.getCell9());
+//            board.set(0, move.getCell1());
+//            board.set(1, move.getCell2());
+//            board.set(2, move.getCell3());
+//            board.set(3, move.getCell4());
+//            board.set(4, move.getCell5());
+//            board.set(5, move.getCell6());
+//            board.set(6, move.getCell7());
+//            board.set(7, move.getCell8());
+//            board.set(8, move.getCell9());
+//        }
+
         if(!check_move(move.getMove(), true)){
+            //move.setNew_board(board);
             return "index?faces-redirect=true";
             //this is the move where the player cell is taken
         }else {
             switch (check_board()){
                 case "1p":
+                    winner = "user!";
                     return "winner?faces-redirect=true";
                 case "2p":
+                    winner ="computer!";
                     return "winner?faces-redirect=true";
                 case "tie":
+                    winner="none";
                     return "winner?faces-redirect=true";
                 default:
+                    set_items();
                     return "index?faces=redirect=true";
             }
         }
@@ -37,11 +81,10 @@ public class functions {
                 }else{
                     if (player){
                         board.set(0, "X");
-                        return true;
                     }else{
                         board.set(0, "O");
-                        return true;
                     }
+                    return true;
                 }
             case "2":
                 if(board.get(1).equals("X") || board.get(1).equals("O")){
@@ -49,11 +92,10 @@ public class functions {
                 }else{
                     if (player){
                         board.set(1, "X");
-                        return true;
                     }else{
                         board.set(1, "O");
-                        return true;
-                    }
+                 }
+                    return true;
                 }
             case "3":
                 if(board.get(2).equals("X") || board.get(2).equals("O")){
@@ -61,11 +103,10 @@ public class functions {
                 }else{
                     if (player){
                         board.set(2, "X");
-                        return true;
                     }else{
                         board.set(2, "O");
-                        return true;
                     }
+                    return true;
                 }
             case "4":
                 if(board.get(3).equals("X") || board.get(3).equals("O")){
@@ -73,11 +114,10 @@ public class functions {
                 }else{
                     if (player){
                         board.set(3, "X");
-                        return true;
                     }else{
                         board.set(3, "O");
-                        return true;
                     }
+                    return true;
                 }
             case "5":
                 if(board.get(4).equals("X") || board.get(4).equals("O")){
@@ -85,11 +125,10 @@ public class functions {
                 }else{
                     if (player){
                         board.set(4, "X");
-                        return true;
                     }else{
                         board.set(4, "O");
-                        return true;
                     }
+                    return true;
                 }
             case "6":
                 if(board.get(5).equals("X") || board.get(5).equals("O")){
@@ -97,11 +136,10 @@ public class functions {
                 }else{
                     if (player){
                         board.set(5, "X");
-                        return true;
                     }else{
                         board.set(5, "O");
-                        return true;
                     }
+                    return true;
                 }
             case "7":
                 if(board.get(6).equals("X") || board.get(6).equals("O")){
@@ -109,11 +147,10 @@ public class functions {
                 }else{
                     if (player){
                         board.set(6, "X");
-                        return true;
                     }else{
                         board.set(6, "O");
-                        return true;
                     }
+                    return true;
                 }
             case "8":
                 if(board.get(7).equals("X") || board.get(7).equals("O")){
@@ -121,11 +158,10 @@ public class functions {
                 }else{
                     if (player){
                         board.set(7, "X");
-                        return true;
                     }else{
                         board.set(7, "O");
-                        return true;
                     }
+                    return true;
                 }
             case "9":
                 if(board.get(8).equals("X") || board.get(8).equals("O")){
@@ -133,11 +169,10 @@ public class functions {
                 }else{
                     if (player){
                         board.set(8, "X");
-                        return true;
                     }else{
                         board.set(8, "O");
-                        return true;
                     }
+                    return true;
                 }
             default:
                 return false;
@@ -147,7 +182,7 @@ public class functions {
     }
 
     public String check_board(){
-        Boolean counter = false;
+        boolean counter = false;
         for(int j=0; j<9;j++){
             if(board.get(j).equals("X") || board.get(j).equals("O")){
                 counter = true;
@@ -214,9 +249,21 @@ public class functions {
                 break;
             }
         }
-
         check_winner(true, true);
+    }
 
+    public String reset_board(){
+        board.set(0, "1");
+        board.set(1, "2");
+        board.set(2, "3");
+        board.set(3, "4");
+        board.set(4, "5");
+        board.set(5, "6");
+        board.set(6, "7");
+        board.set(7, "8");
+        board.set(8, "9");
+
+        return "index?faces=redirect=true";
     }
 
     public List<String> getBoard() {
@@ -235,7 +282,7 @@ public class functions {
         this.winner = winner;
     }
 
-    public backend getMove() {
+    public  backend getMove() {
         return move;
     }
 
